96f6ed15d7d0d2d6e2fefe6b8ffc98b9efb20e71
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2020-11-22 00:23:59 -0800

    Remove "what" section of test matrix. (#239)
    We were generating two whole sets of jobs, for --tests and --docs
    separately. But the default invocation of cargo runs both tests and
    doctests. We can save some resources and speed up our test runs by
    allowing cargo to do both in one invocation.

--- test.sh
+++ test.sh
@@ -6,11 +6,9 @@ export RUSTFLAGS="-D dead_code -D unused-variables -D unused"
 
 for tls in "" tls ; do
   for feature in "" json charset cookies socks-proxy ; do
-    for what in --doc --tests ; do
-      if ! cargo test "${what}" --no-default-features --features "${tls} ${feature}" ; then
-        echo Command failed: cargo test \"${what}\" --no-default-features --features \"${tls} ${feature}\"
-        exit 1
-      fi
-    done
+    if ! cargo test --no-default-features --features "${tls} ${feature}" ; then
+      echo Command failed: cargo test \"${what}\" --no-default-features --features \"${tls} ${feature}\"
+      exit 1
+    fi
   done
 done
