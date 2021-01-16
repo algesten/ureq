d627ef9704c4dc7e0fb9e2d01cec1f0c7022cb35
Martin Algesten <martin@algesten.se>
2021-01-16 10:54:11 +0100

    OrAnyStatus::or_any_status ergonomic helper
    Some users might prefer to handle all HTTP responses as Response
    regardless of status code.

--- src/lib.rs
+++ src/lib.rs
@@ -293,7 +293,7 @@ mod testserver;
 
 pub use crate::agent::Agent;
 pub use crate::agent::AgentBuilder;
-pub use crate::error::{Error, ErrorKind, Transport};
+pub use crate::error::{Error, ErrorKind, OrAnyStatus, Transport};
 pub use crate::header::Header;
 pub use crate::proxy::Proxy;
 pub use crate::request::Request;
