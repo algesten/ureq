c4c161804288d8505b3a2287fc6916f3059e51a5
Martin Algesten <martin@algesten.se>
2021-01-26 20:08:55 +0100

    Remove unnecessary cloning in Request::do_call()

--- src/request.rs
+++ src/request.rs
@@ -111,19 +111,19 @@ impl Request {
         self.do_call(Payload::Empty)
     }
 
-    fn do_call(&self, payload: Payload) -> Result<Response> {
+    fn do_call(self, payload: Payload) -> Result<Response> {
         for h in &self.headers {
             h.validate()?;
         }
-        let mut url: Url = match self.url.clone() {
+        let mut url: Url = match self.url {
             Urlish::Url(u) => u,
-            Urlish::Str(s) => s.parse().map_err(|e: url::ParseError| {
+            Urlish::Str(s) => s.parse().map_err(|e| {
                 ErrorKind::InvalidUrl
-                    .msg(&format!("failed to parse URL '{}'", self.url))
+                    .msg(&format!("failed to parse URL: {:?}", e))
                     .src(e)
             })?,
         };
-        for (name, value) in self.query_params.clone() {
+        for (name, value) in self.query_params {
             url.query_pairs_mut().append_pair(&name, &value);
         }
         let deadline = match self.timeout.or(self.agent.config.timeout) {
