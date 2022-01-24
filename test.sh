ca5b8bcbaf7aff9ba99bc9939695dd92c3bc3e3f
Pablo Mansanet <pablomansanet@gmail.com>
2022-01-24 12:10:39 +0100

    Add new feature flag to test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ set -eu
 export RUST_BACKTRACE=1
 export RUSTFLAGS="-D dead_code -D unused-variables -D unused"
 
-for feature in "" tls json charset cookies socks-proxy "tls native-certs" native-tls; do
+for feature in "" tls json charset cookies socks-proxy "tls native-certs" native-tls digest-auth; do
   if ! cargo test --no-default-features --features "${feature}" ; then
     echo Command failed: cargo test --no-default-features --features \"${feature}\"
     exit 1
