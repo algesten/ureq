189ea607a49ceb7d3ae210c203794d261f0fb090
Kade Robertson <kade@kaderobertson.dev>
2023-02-13 16:53:31 -0500

    ci: fix http feature name

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ set -eu
 export RUST_BACKTRACE=1
 export RUSTFLAGS="-D dead_code -D unused-variables -D unused"
 
-for feature in "" tls json charset cookies socks-proxy "tls native-certs" native-tls gzip brotli http; do
+for feature in "" tls json charset cookies socks-proxy "tls native-certs" native-tls gzip brotli http-interop; do
   if ! cargo test --no-default-features --features "${feature}" ; then
     echo Command failed: cargo test --no-default-features --features \"${feature}\"
     exit 1
