algesten/ureq

commit 4602c6567a923e3e1e4b3fe447ffa9d489302759

fix: use reader for bytes http response transformation
The From implementation to transform a ureq::Response into an
http::Response<Vec<u8>> is using the into_string() method. This method
only allows for UTF-8 characters in the body payload, thus the
result is inconsistent for non UTF-8 characters like protobuffers (binary).

The proposed solution uses the reader instead of transforming the input
into a UTF-8 character.

src/http_crate.rs
@@ -122,9 +122,10 @@ impl From<Response> for http::Response<String> {
 /// ```
 impl From<Response> for http::Response<Vec<u8>> {
     fn from(value: Response) -> Self {
-        create_builder(&value)
-            .body(value.into_string().unwrap().into_bytes())
-            .unwrap()
+        let respone_builder = create_builder(&value);
+        let mut body_buf: Vec<u8> = vec![];
+        value.into_reader().read_to_end(&mut body_buf).unwrap();
+        respone_builder.body(body_buf).unwrap();
     }
 }
 
@@ -331,12 +332,15 @@ mod tests {
         use std::io::{Cursor, Read};
 
         let mut response = super::Response::new(200, "OK", "tbr").unwrap();
-        response.reader = Box::new(Cursor::new(vec![0xde, 0xad, 0xbe, 0xef]));
-        let http_response: Response<Box<dyn Read + Send + Sync + 'static>> = response.into();
+        // b'\xFF' as invalid UTF-8 character
+        response.reader = Box::new(Cursor::new(vec![b'\xFF', 0xde, 0xad, 0xbe, 0xef]));
+        let http_response: Response<Vec<u8>> = response.into();
 
-        let mut buf = vec![];
-        http_response.into_body().read_to_end(&mut buf).unwrap();
-        assert_eq!(buf, vec![0xde, 0xad, 0xbe, 0xef]);
+        assert_eq!(
+            http_response.body().to_vec(),
+            // non UTF-8 byte is preserved
+            vec![b'\xFF', 0xde, 0xad, 0xbe, 0xef]
+        );
     }
 
     #[test]

src/http_interop.rs
@@ -128,9 +128,10 @@ impl From<Response> for http::Response<String> {
 /// ```
 impl From<Response> for http::Response<Vec<u8>> {
     fn from(value: Response) -> Self {
-        create_builder(&value)
-            .body(value.into_string().unwrap().into_bytes())
-            .unwrap()
+        let respone_builder = create_builder(&value);
+        let mut body_buf: Vec<u8> = vec![];
+        value.into_reader().read_to_end(&mut body_buf).unwrap();
+        respone_builder.body(body_buf).unwrap()
     }
 }
 
@@ -371,15 +372,18 @@ mod tests {
     #[test]
     fn convert_to_http_response_bytes() {
         use http::Response;
-        use std::io::{Cursor, Read};
+        use std::io::Cursor;
 
         let mut response = super::Response::new(200, "OK", "tbr").unwrap();
-        response.reader = Box::new(Cursor::new(vec![0xde, 0xad, 0xbe, 0xef]));
-        let http_response: Response<Box<dyn Read + Send + Sync + 'static>> = response.into();
+        // b'\xFF' as invalid UTF-8 character
+        response.reader = Box::new(Cursor::new(vec![b'\xFF', 0xde, 0xad, 0xbe, 0xef]));
+        let http_response: Response<Vec<u8>> = response.into();
 
-        let mut buf = vec![];
-        http_response.into_body().read_to_end(&mut buf).unwrap();
-        assert_eq!(buf, vec![0xde, 0xad, 0xbe, 0xef]);
+        assert_eq!(
+            http_response.body().to_vec(),
+            // non UTF-8 byte is preserved
+            vec![b'\xFF', 0xde, 0xad, 0xbe, 0xef]
+        );
     }
 
     #[test]

commit a3e9aa2e13f688545a15ca18b02bd7ee44bedf8a

Fix compilation error introduced in 4602c65

src/http_crate.rs
@@ -125,7 +125,7 @@ impl From<Response> for http::Response<Vec<u8>> {
         let respone_builder = create_builder(&value);
         let mut body_buf: Vec<u8> = vec![];
         value.into_reader().read_to_end(&mut body_buf).unwrap();
-        respone_builder.body(body_buf).unwrap();
+        respone_builder.body(body_buf).unwrap()
     }
 }
 