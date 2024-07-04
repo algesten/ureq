a9b0ea4c28f7a124075fedefb2b389b6c234fd43
Martin Algesten <martin@algesten.se>
2024-07-04 13:31:07 +0200

    Start of internal Sans-IO polling

--- src/unit.rs
+++ src/unit.rs
@@ -1,55 +1,55 @@
-use hoot::client::flow::state::{Prepare, SendRequest};
-use hoot::client::flow::{Flow, SendRequestResult};
-use http::{Request, Response};
+use std::net::SocketAddr;
+use std::time::Instant;
 
-use crate::body::RecvBody;
-use crate::pool::{Connection, ConnectionPool};
+use hoot::client::flow::Flow;
+use http::{Request, Uri};
+
+use crate::flow::FlowHolder;
 use crate::{Body, Error};
 
-pub(crate) struct Unit;
+pub(crate) struct Unit<'a, B> {
+    start_time: Instant,
+    flow: FlowHolder<'a, B>,
+}
 
-impl Unit {
-    pub(crate) fn run(
-        &mut self,
-        pool: &mut ConnectionPool,
-        request: &Request<impl Body>,
-    ) -> Result<Response<RecvBody>, Error> {
+pub enum Output<'a> {
+    Reset,
+    DnsLookup {
+        uri: &'a Uri,
+        timeout: Instant,
+    },
+    OpenConnection {
+        uri: &'a Uri,
+        addr: SocketAddr,
+        timeout: Instant,
+    },
+    Transmit {
+        amount: usize,
+        timeout: Instant,
+    },
+    AwaitInput {
+        timeout: Instant,
+    },
+}
+
+pub enum Input<'a> {
+    SocketAddr(SocketAddr),
+    ConnectionOpen,
+    Input { data: &'a [u8] },
+}
+
+impl<'a, B: Body> Unit<'a, B> {
+    pub fn new(start_time: Instant, request: &'a Request<B>) -> Result<Self, Error> {
         let flow = Flow::new(request)?;
 
-        self.prepare(pool, flow)?;
+        let flow = FlowHolder::Prepare(flow);
 
+        Ok(Self { start_time, flow })
+    }
+
+    pub fn poll_output(&'a mut self, transmit_buffer: &mut [u8]) -> Output {
         todo!()
     }
 
-    fn prepare<B>(
-        &mut self,
-        pool: &mut ConnectionPool,
-        flow: Flow<B, Prepare>,
-    ) -> Result<(), Error> {
-        let connection = pool.connect(flow.uri())?;
-
-        self.send_request(flow.proceed(), connection)
-    }
-
-    fn send_request<B>(
-        &self,
-        mut flow: Flow<B, SendRequest>,
-        mut connection: Connection,
-    ) -> Result<(), Error> {
-        loop {
-            if flow.can_proceed() {
-                break;
-            }
-
-            let mut buffer = connection.output_buffer();
-            let n = flow.write(buffer.as_mut())?;
-            buffer.flush(n)?;
-        }
-
-        match flow.proceed().unwrap() {
-            SendRequestResult::Await100(_) => todo!(),
-            SendRequestResult::SendBody(_) => todo!(),
-            SendRequestResult::RecvResponse(_) => todo!(),
-        }
-    }
+    pub fn handle_input(&mut self, now: Instant, input: Input) {}
 }
