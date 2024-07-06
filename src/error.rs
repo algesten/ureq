0d500b54701e8d7d30727fc09e452419007343e6
Martin Algesten <martin@algesten.se>
2024-07-06 21:14:57 +0200

    Body writing

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,12 @@
+use std::io;
+
 use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum Error {
     #[error("protocol: {0}")]
     Protocol(#[from] hoot::Error),
+
+    #[error("io: {0}")]
+    Io(#[from] io::Error),
 }
