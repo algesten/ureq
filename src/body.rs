1b9cb3bf1d7d19a1e346b73f4b889029b9a9b9b4
Martin Algesten <martin@algesten.se>
2024-07-06 10:57:49 +0200

    More body woes

--- src/body.rs
+++ src/body.rs
@@ -1,21 +1,24 @@
 // TODO(martin): Is a better name SendBody to  mirror RecvBody? Or even invert
 // BodySend, BodyRecv to get alphabetical sorting.
 
+use std::fs::File;
 use std::io::Read;
 
 pub struct Body<'a> {
     inner: BodyInner<'a>,
+    ended: bool,
 }
 
 impl<'a> Body<'a> {
     pub fn empty() -> Body<'static> {
         Body {
             inner: BodyInner::ByteSlice(&[]),
+            ended: false,
         }
     }
 }
 
-pub trait IntoBody {
+pub trait AsBody {
     fn as_body(&self) -> Body;
 }
 
@@ -24,16 +27,36 @@ pub(crate) enum BodyInner<'a> {
     Reader(&'a mut dyn Read),
 }
 
-impl IntoBody for &[u8] {
+macro_rules! impl_into_body {
+    ($t:ty) => {
+        impl AsBody for $t {
+            fn as_body(&self) -> Body {
+                BodyInner::ByteSlice(self.as_ref()).into()
+            }
+        }
+    };
+}
+
+impl AsBody for &mut File {
     fn as_body(&self) -> Body {
-        BodyInner::ByteSlice(self).into()
+        unreachable!()
     }
 }
+
+impl_into_body!(&[u8]);
+impl_into_body!(&str);
+impl_into_body!(String);
+impl_into_body!(Vec<u8>);
+impl_into_body!(&String);
+impl_into_body!(&Vec<u8>);
 
 pub struct RecvBody;
 
 impl<'a> From<BodyInner<'a>> for Body<'a> {
     fn from(inner: BodyInner<'a>) -> Self {
-        Body { inner }
+        Body {
+            inner,
+            ended: false,
+        }
     }
 }
