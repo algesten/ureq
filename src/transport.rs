329786815ac7584807e016baa10814a961680705
Martin Algesten <martin@algesten.se>
2024-07-07 10:45:40 +0200

    poll/handle driving of unit

--- src/transport.rs
+++ src/transport.rs
@@ -1,14 +1,23 @@
 use std::fmt::Debug;
+use std::net::SocketAddr;
+use std::time::Duration;
 
 use http::Uri;
 
 use crate::Error;
 
 pub trait Transport: Debug + 'static {
-    fn connect(&mut self, uri: &Uri) -> Result<Box<dyn Conn>, Error>;
+    fn connect(
+        &mut self,
+        uri: &Uri,
+        addr: SocketAddr,
+        timeout: Duration,
+    ) -> Result<Box<dyn Socket>, Error>;
 }
 
-pub trait Conn: Debug {
-    fn output_buffer(&mut self) -> &mut [u8];
-    fn output_buffer_flush(&mut self, amount: usize) -> Result<(), Error>;
+pub trait Socket: Debug {
+    fn buffer_borrow(&mut self) -> &mut [u8];
+    fn buffer_transmit(&mut self, amount: usize, timeout: Duration) -> Result<(), Error>;
+    fn input_await(&mut self, timeout: Duration) -> Result<&[u8], Error>;
+    fn input_consume(&mut self, amount: usize);
 }
