360afbde0b1e081b48fbebdcb1952b401ed193ab
Martin Algesten <martin@algesten.se>
2024-07-13 15:08:33 +0200

    Move LimitReader to limit.rs

--- src/body/mod.rs
+++ src/body/mod.rs
@@ -6,12 +6,14 @@ use crate::time::Instant;
 use crate::unit::Unit;
 use crate::Error;
 
-use self::handler::{UnitHandler, UnitHandlerRef};
+use self::handler::UnitHandler;
+use self::limit::LimitReader;
+
+mod handler;
+mod limit;
 
 #[cfg(feature = "charset")]
 mod charset;
-
-mod handler;
 
 pub struct Body {
     info: ResponseInfo,
@@ -250,49 +252,6 @@ impl<R: Read> Read for ContentDecoder<R> {
             ContentDecoder::Brotli(v) => v.read(buf),
             ContentDecoder::PassThrough(v) => v.read(buf),
         }
-    }
-}
-
-struct LimitReader<'a> {
-    unit_handler: UnitHandlerRef<'a>,
-    left: u64,
-}
-
-impl<'a> LimitReader<'a> {
-    fn shared(u: &'a mut UnitHandler, limit: u64) -> LimitReader<'a> {
-        Self {
-            unit_handler: UnitHandlerRef::Shared(u),
-            left: limit,
-        }
-    }
-}
-
-impl LimitReader<'static> {
-    fn owned(u: UnitHandler, limit: u64) -> LimitReader<'static> {
-        Self {
-            unit_handler: UnitHandlerRef::Owned(u),
-            left: limit,
-        }
-    }
-}
-
-impl<'a> Read for LimitReader<'a> {
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        if self.left == 0 {
-            return Err(Error::BodyExceedsLimit.into_io());
-        }
-
-        // The max buffer size is usize, which may be 32 bit.
-        let max = (self.left.min(usize::MAX as u64) as usize).min(buf.len());
-
-        let n = self
-            .unit_handler
-            .do_read(&mut buf[..max])
-            .map_err(|e| e.into_io())?;
-
-        self.left -= n as u64;
-
-        Ok(n)
     }
 }
 
