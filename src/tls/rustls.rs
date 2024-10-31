bc029c26548857fca47145d4043ca7fc2c747c75
Sosthène Guédon <sosthene@nitrokey.com>
2024-10-31 15:31:07 +0100

    disable_verification: Signal that all signature schemes are supported
    Without this, the server will not accept the connection because it can't
    use any signature scheme

--- src/tls/rustls.rs
+++ src/tls/rustls.rs
@@ -240,7 +240,20 @@ impl ServerCertVerifier for DisabledVerifier {
     }
 
     fn supported_verify_schemes(&self) -> Vec<rustls::SignatureScheme> {
-        vec![]
+        vec![
+            rustls::SignatureScheme::RSA_PKCS1_SHA1,
+            rustls::SignatureScheme::RSA_PKCS1_SHA256,
+            rustls::SignatureScheme::RSA_PKCS1_SHA384,
+            rustls::SignatureScheme::RSA_PKCS1_SHA512,
+            rustls::SignatureScheme::ECDSA_NISTP256_SHA256,
+            rustls::SignatureScheme::ECDSA_NISTP384_SHA384,
+            rustls::SignatureScheme::ECDSA_NISTP521_SHA512,
+            rustls::SignatureScheme::RSA_PSS_SHA256,
+            rustls::SignatureScheme::RSA_PSS_SHA384,
+            rustls::SignatureScheme::RSA_PSS_SHA512,
+            rustls::SignatureScheme::ED25519,
+            rustls::SignatureScheme::ED448,
+        ]
     }
 }
 
