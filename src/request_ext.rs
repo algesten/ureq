07b8ea28d3392e48fb454a847608f9653e81e423
Martin Algesten <martin@algesten.se>
2026-08-08 22:15:57 +0200

    Seal RequestExt (#1189)

--- src/request_ext.rs
+++ src/request_ext.rs
@@ -6,11 +6,15 @@ use crate::{Agent, AsSendBody, Body, Error, http};
 use std::ops::Deref;
 use ureq_proto::http::{Request, Response};
 
+mod private {
+    pub trait Sealed {}
+}
+
 /// Extension trait for [`http::Request<impl AsSendBody>`].
 ///
 /// Adds additional convenience methods to the `Request` that are not available
 /// in the plain http API.
-pub trait RequestExt<S>
+pub trait RequestExt<S>: private::Sealed
 where
     S: AsSendBody,
 {
@@ -153,6 +157,8 @@ pub enum AgentRef<'a> {
     Owned(Agent),
     Borrowed(&'a Agent),
 }
+
+impl<S: AsSendBody> private::Sealed for http::Request<S> {}
 
 impl<S: AsSendBody> RequestExt<S> for http::Request<S> {
     fn with_agent<'a>(self, agent: impl Into<AgentRef<'a>>) -> WithAgent<'a, S> {
